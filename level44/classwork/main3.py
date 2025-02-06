4202574b71456408ac6f7785f7f18c0684f9cf7d
luka353 <lukacixelashvili22@gmail.com>
2025-02-06 14:46:44 +0400

    level44

---
+++ level44/classwork/main3.py
@@ -0,0 +1,2 @@
+def twice_as_old(father_age, son_age):
+    return abs(father_age - 2 * son_age)
---
+++ level44/classwork/main4.py
@@ -0,0 +1,31 @@
+def greet(language):
+    if not isinstance(language, str):
+        return "Welcome"
+    
+    language_db = [
+        ("english", "Welcome"),
+        ("czech", "Vitejte"),
+        ("danish", "Velkomst"),
+        ("dutch", "Welkom"),
+        ("estonian", "Tere tulemast"),
+        ("finnish", "Tervetuloa"),
+        ("flemish", "Welgekomen"),
+        ("french", "Bienvenue"),
+        ("german", "Willkommen"),
+        ("irish", "Failte"),
+        ("italian", "Benvenuto"),
+        ("latvian", "Gaidits"),
+        ("lithuanian", "Laukiamas"),
+        ("polish", "Witamy"),
+        ("spanish", "Bienvenido"),
+        ("swedish", "Valkommen"),
+        ("welsh", "Croeso")
+    ]
+    
+    language = language.lower()
+    
+    for lang, greeting in language_db:
+        if lang == language:
+            return greeting
+    
+    return "Welcome"
